2c5f6ecc282a340763b6d1c9cdbcb47027c46542
Labbouyat <labbouyat@gmail.com>
2021-12-28 11:14:58 -0500

    AreaOfCircle completed

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             double radius, area;
+             Console.WriteLine("What is the radius of the circle?");
+             radius = Convert.ToDouble(Console.ReadLine());
+
+             area = Math.PI * radius *radius;
+
+             Console.WriteLine("Area of the Circle with radius {0} is {1}",radius,area);
+
+             Console.WriteLine("Press any Key to continue...");
+             Console.ReadLine();
+        }
+    }
+}
